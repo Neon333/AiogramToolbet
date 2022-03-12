3aa6a650371f580509bb8bae74249e67d314b90c
Neon333 <rixibike@gmail.com>
2022-03-12 19:53:13 +0300

    added kwargs to render_menu wrapper

--- menu/helper.py
+++ menu/helper.py
@@ -5,9 +5,9 @@ from . import MenuType
 
 
 def render_menu(menu_cls: MenuType, integrate: bool = False):
-    async def _wrapper(initiator_message: Message, state: FSMContext):
-        return await menu_cls.render(initiator_message, state) if not integrate else await menu_cls.render_in(
-            initiator_message, state
+    async def _wrapper(initiator_message: Message, state: FSMContext, **kwargs):
+        return await menu_cls.render(initiator_message, state, **kwargs) if not integrate else await menu_cls.render_in(
+            initiator_message, state, **kwargs
         )
 
     return _wrapper
